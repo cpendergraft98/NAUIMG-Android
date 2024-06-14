5ab5c0f8f2186e130ceab61c8cb09d8854983059
Cole Pendergraft <colepend98@gmail.com>
2024-06-14 11:22:27 -0700

    Minor Updates
    Added comments to Kotlin code for legibility and used updated methodology for Return button.

--- app/src/main/java/com/example/nauimg/MainActivity.kt
+++ app/src/main/java/com/example/nauimg/MainActivity.kt
@@ -13,15 +13,19 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import android.widget.ArrayAdapter
 
+// MainActivity class extends AppCompatActivity
 class MainActivity : AppCompatActivity() {
+    // Initialize variables
     private lateinit var gameSpinner: Spinner
     private lateinit var launchButton: Button
     private var selectedGame: String? = null
 
+    // Called when the activity is first created
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Find views by their IDs
         gameSpinner = findViewById(R.id.gameSpinner)
         launchButton = findViewById(R.id.launchButton)
 
@@ -34,24 +38,31 @@ class MainActivity : AppCompatActivity() {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         gameSpinner.adapter = adapter
 
+        // Set item selected listener for spinner
         gameSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            // Called when item in spinner is selected
             override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long){
                 selectedGame = parent.getItemAtPosition(position) as String
             }
 
+            // Called when no item is selected in spinner
             override fun onNothingSelected(parent: AdapterView<*>?) {
                 selectedGame = null
             }
         }
 
+        // Set click listener for launch button
         launchButton.setOnClickListener {
             selectedGame?.let {
+                // Create intent to start WebViewActivity with selected game file
                 val intent = Intent(this, WebViewActivity::class.java).apply{
                     putExtra("FILENAME", it)
                 }
                 startActivity(intent)
             }
         }
+
+        // Request location permission if not granted
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
             != PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(this, arrayOf
--- app/src/main/java/com/example/nauimg/WebViewActivity.kt
+++ app/src/main/java/com/example/nauimg/WebViewActivity.kt
@@ -17,47 +17,58 @@ import android.location.Location
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import android.Manifest
+import android.annotation.SuppressLint
 import android.util.Log
+import android.widget.Button
 import com.google.android.gms.location.Priority
 import com.google.android.gms.location.LocationRequest
 
+// WebViewActivity class extends AppCompatActivity
 class WebViewActivity : AppCompatActivity() {
+    // Initialize variables
     private lateinit var webView: WebView
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var locationRequest: LocationRequest
     private lateinit var locationCallback: LocationCallback
     private var latestLocation: Location? = null
+    private lateinit var returnButton: Button
 
+    // Called when the activity is first created
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web_view)
 
+        // Find views by their IDs
         webView = findViewById(R.id.webView)
+        returnButton = findViewById(R.id.returnButton)
 
         // Initialize Location Client
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
         // Create location request
-        locationRequest = LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 5000).apply{
+        locationRequest = LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 5000).apply {
             setMinUpdateIntervalMillis(5000)
             setMaxUpdateDelayMillis(5000)
         }.build()
 
-        locationCallback = object: LocationCallback() {
+        locationCallback = object : LocationCallback() {
+            // Called when a new location update is received
             override fun onLocationResult(locationResult: LocationResult) {
                 super.onLocationResult(locationResult)
                 latestLocation = locationResult.lastLocation
             }
         }
 
-        // Check for location permissions
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+        // Check for location permissions and start location updates
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), Constants.PERMISSIONS_REQUEST_LOCATION)
         } else {
             startLocationUpdates()
         }
 
+        // Configure WebView settings
         val webSettings = webView.settings
+        @SuppressLint("SetJavaScriptEnabled") // The warning for JavaScriptEnabled is a nonissue
         webSettings.javaScriptEnabled = true
         webSettings.javaScriptCanOpenWindowsAutomatically = true
         webSettings.allowFileAccess = true
@@ -65,23 +76,32 @@ class WebViewActivity : AppCompatActivity() {
         webSettings.loadWithOverviewMode = true
         webSettings.useWideViewPort = true
 
+        // Set WebChromeClient for handling geolocation permissions
         webView.webChromeClient = object : WebChromeClient() {
-            override fun onGeolocationPermissionsShowPrompt(
-                origin: String, callback: GeolocationPermissions.Callback){
+            override fun onGeolocationPermissionsShowPrompt(origin: String, callback: GeolocationPermissions.Callback) {
                 callback.invoke(origin, true, false)
             }
         }
 
+        // Load HTML file specified in the intent
         val filename = intent.getStringExtra("FILENAME")
         if (filename != null) {
             webView.webViewClient = WebViewClient()
             webView.loadUrl("file:///android_asset/$filename")
         }
 
-        // Inject the interface into WebView
+        // Inject Javascript interfaces into WebView
         webView.addJavascriptInterface(this, "AndroidBridge")
+
+        // Set click listener for return button
+        returnButton.setOnClickListener {
+            // Create intent to return to MainActivity
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 
+    // Start location updates if permission is granted
     private fun startLocationUpdates() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             fusedLocationClient.requestLocationUpdates(locationRequest, ContextCompat.getMainExecutor(this), locationCallback)
@@ -91,10 +111,12 @@ class WebViewActivity : AppCompatActivity() {
         }
     }
 
-    private fun stopLocationUpdates(){
+    // Stop location updates
+    private fun stopLocationUpdates() {
         fusedLocationClient.removeLocationUpdates(locationCallback)
     }
 
+    // Handle permission request result
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == Constants.PERMISSIONS_REQUEST_LOCATION) {
@@ -104,34 +126,38 @@ class WebViewActivity : AppCompatActivity() {
         }
     }
 
+    // Stop location updates onPause
     override fun onPause() {
         super.onPause()
         stopLocationUpdates()
     }
 
+    // Start location updates onResume if permission is granted
     override fun onResume() {
         super.onResume()
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             startLocationUpdates()
         }
     }
 
-    // Ignore warning that view is not used, btnReturnHandler does not work without it and I do not
-    // know why
-    fun btnReturnHandler(view: View){
+    /*
+    // Button click handler to return to MainActivity
+    fun btnReturnHandler(view: View) {
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
     }
+     */
 
+    // Generate random metrics and return as JSON string
     @JavascriptInterface
-    fun generateMetrics(): String{
+    fun generateMetrics(): String {
         val random = Random()
 
-        val uploadSpeed = random.nextInt((100 - 1) + 1) + 1 //Mbs
-        val downloadSpeed = random.nextInt((100 - 1) + 1) + 1 //Mbs
-        val jitter = random.nextInt((50 - 1) + 1) + 1 //ms
-        val packetLoss = random.nextInt(100) //%
-        val latency = random.nextInt((100 - 1) + 1) + 1 //ms
+        val uploadSpeed = random.nextInt((100 - 1) + 1) + 1 // Mbps
+        val downloadSpeed = random.nextInt((100 - 1) + 1) + 1 // Mbps
+        val jitter = random.nextInt((50 - 1) + 1) + 1 // ms
+        val packetLoss = random.nextInt(100) // %
+        val latency = random.nextInt((100 - 1) + 1) + 1 // ms
 
         val metricJSON = JSONObject()
         metricJSON.put("uploadSpeed", uploadSpeed)
@@ -143,6 +169,7 @@ class WebViewActivity : AppCompatActivity() {
         return metricJSON.toString()
     }
 
+    // Get latest location and return as JSON string
     @JavascriptInterface
     fun getLocationJSON(): String {
         val locationJSON = JSONObject()

bc2debe766bb0dfdd4b1bce35c931daecb786d38
Cole Pendergraft <colepend98@gmail.com>
2024-07-10 11:30:55 -0700

    Implemented twine-side data logging
    Whenever the internet speed measurement function runs in the background, the data associated with that check is stored locally on the user device.

--- app/src/main/java/com/example/nauimg/WebAppInterface.kt
+++ app/src/main/java/com/example/nauimg/WebAppInterface.kt
@@ -2,8 +2,10 @@ package com.example.nauimg
 
 import android.content.Context
 import android.webkit.JavascriptInterface
+import org.json.JSONArray
 import org.json.JSONObject
 import java.util.Random
+import java.io.File
 
 // WebAppInterface and related methods for communicating between Androind and JS
 class WebAppInterface(private val context: Context) {
@@ -33,6 +35,35 @@ class WebAppInterface(private val context: Context) {
     fun getLocationJSON(): String {
         // Return the current state of the JSON array as a formatted string
         return LocationService.locationData.toString(4)
+    }
+
+    @JavascriptInterface
+    fun writeTwineData(data: String){
+        // Parse the received JSON string into a JSONObject
+        val jsonObject = JSONObject(data)
+
+        // Path to the file
+        val file = File(context.filesDir, "check_data.json")
+
+        val jsonArray: JSONArray
+
+        // Check if the file exists
+        if(file.exists()){
+            // Read the existing file
+            val existingData = file.readText()
+            // Parse the existing file content into a JSONArray
+            jsonArray = JSONArray(existingData)
+        }else{
+            // Create a new JSONArray
+            jsonArray = JSONArray()
+        }
+
+        // Add the new data to the array
+        jsonArray.put(jsonObject)
+
+        // Write the updated array back to the file
+        file.writeText(jsonArray.toString())
+
     }
 
 }
